2c87710f2a90f4c13e360761414d30ca5c2161e8
Logan <loganwswenson@gmail.com>
2019-06-29 11:46:19 -0600

    updated buttons

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,8 @@
 
-$('#option_1').prepend('<img id="theImg" src="assets/images/dino-jirf.jpg" style="width:100px;height:100px;"/>')
-$('#option_2').prepend('<img id="theImg" src="assets/images/fly-dino.jpg" style="width:100px;height:100px;"/>')
+$('#option_1')
+$('#option_2')
+$('#option_3')
+$('#option_4')
+
+//track image selection
+//
