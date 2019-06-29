e0fa44b996047658ff0f70484938ef602b5fa713
Joby <janthonywolfe@gmail.com>
2019-06-29 10:47:41 -0600

    save me

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC5oiOA7TPZ-bh4y0DhUPj-o4rAIxz8tO8",
+    authDomain: "mood-quiz.firebaseapp.com",
+    databaseURL: "https://mood-quiz.firebaseio.com",
+    projectId: "mood-quiz",
+    storageBucket: "",
+    messagingSenderId: "296482666275",
+    appId: "1:296482666275:web:9764064bf1a80f75"
+  };
+
+    firebase.initializeApp(firebaseConfig);
+    
+    let database = firebase.database();
+
+    
