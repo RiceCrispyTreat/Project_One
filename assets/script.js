a8b09f435ed5f5c876a46ef7035631a46b76ef8c
Logan <loganwswenson@gmail.com>
2019-06-29 10:24:54 -0600

    buttons added

---
+++ assets/script.js
@@ -0,0 +1,3 @@
+
+$('#option_1').prepend('<img id="theImg" src="assets/images/dino-jirf.jpg" style="width:100px;height:100px;"/>')
+$('#option_2').prepend('<img id="theImg" src="assets/images/fly-dino.jpg" style="width:100px;height:100px;"/>')
