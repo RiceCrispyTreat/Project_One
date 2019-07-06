96cc704ce5d3597b5c800ab74a2beb44e493afd9
Joby <janthonywolfe@gmail.com>
2019-07-05 23:01:33 -0600

    added movie to DOM

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,4 @@
-//This is the instructions/start of the quiz
-
-// let instructions = $('#instructions')
-// let startGame = $('<button id="subBtn">Get Started</button>')
-// instructions.text('Please select the following picutres that you connect with').append(startGame)
-// hide(quiz);
-
-
-//Joeys firebase stufffff
+//firebase key
 var firebaseConfig = {
   apiKey: "AIzaSyC5oiOA7TPZ-bh4y0DhUPj-o4rAIxz8tO8",
   authDomain: "mood-quiz.firebaseapp.com",
@@ -23,156 +15,173 @@ let database = firebase.database();
 
 
 // OMDb API key & AJAX
-let adventureList = ['Indiana Jones', 'Inglourious Basterds', 'Back to the Future' , 'O Brother, Where Art Thou', 'Big Fish', 'Spirited Away'];
+let adventureList = ['Indiana Jones', 'Inglourious Basterds', 'Back to the Future', 'O Brother, Where Art Thou', 'Big Fish', 'Spirited Away'];
 let actionList = ['Die Hard', 'Rambo', 'Predator', 'Last Action Hero', 'Aliens', 'Baby Driver'];
 let comedyList = ['Dumb and Dumber', 'Better Off Dead', 'Austin Powers', 'Shrek', 'The Other Guys', 'Elf'];
 let romanceList = ['The Notebook', 'P.S. I Love You', 'Mama Mia', 'Pretty In Pink', 'Sleepless In Seattle', 'Love Actually'];
-let horrorList = ['The Shining', 'The Exorcist', 'The Conjuring', 'Paranormal Activity', 'The Thing', 'The Blair Witch']
+let horrorList = ['The Shining', 'The Exorcist', 'The Conjuring', 'Paranormal Activity', 'The Thing', 'The Blair Witch'];
 
-function movieFind(genre){
-  var searchArray;
-  switch (genre) {
-    default:
-      searchArray = adventureList;
-      break;
-      case "Romance":    
-      searchArray = romanceList;
-      break;
-      case "action":
-        searchArray = actionList;
-        break;
-        case "comedy":
-          searchArray = comedyList;
-          break;
-          case "horror":
-            searchArray =horrorList;
-          }
-          
-          let queryURL = 'https://www.omdbapi.com/?t=' +  + '&apikey=d56ade4e';
-          
-          $.ajax({
-            url: queryURL,
-            method: 'GET'
-          }).then(function(results) {
-            // console.log(results);
-          });
-        }
-        
-        //$('#movie-button').on('click', dasuGudFunction);
-        
-        // movieFind(romanceList[0]);
-        
-        //variables set at 0 for each genre
-        let adventureCount = 0;
-        let comedyCount = 0;
-        let actionCount = 0;
-        let romanceCount = 0;
-        let horrorCount = 0;
-        
-        let genre = undefined;
-        
-        //functions that increase the value of their genre
-        function adventurePlus () {
-          adventureCount++;
-          console.log("adventureCount: " + adventureCount);
-        }
-        
-        function comedyPlus () {
-          comedyCount++;
-          console.log("comedyCount: " + comedyCount);
-        }
-        
-        function actionPlus () {
-          actionCount++;
-          console.log("actionCount: " + actionCount);
-        }
-        
-        function romancePlus () {
-          romanceCount++;
-          console.log("romanceCount: " + romanceCount);
-        }
-        
-        function horrorPlus () {
-          horrorCount++;
-          console.log("horrorCount: " + horrorCount);
-        }
-        
-        //function that disables button from being clicked more than once
-        function stopClick() {
-          $(this).prop('disabled', true);
-        }
-        
-        //function that colors buttons when touched
-        function toggle() {
-          $(this).css('opacity','0.2');
-        }
-        
-        
-        //functions called on click
-        $('#adventure, #adventure2').click(adventurePlus).click(toggle).click(stopClick);
-        $('#action, #action2').click(actionPlus).click(toggle).click(stopClick);
-        $('#comedy, #comedy2').click(comedyPlus).click(toggle).click(stopClick);
-        $('#romance, #romance2').click(romancePlus).click(toggle).click(stopClick);
-        $('#horror, #horror2').click(horrorPlus).click(toggle).click(stopClick);
-        
-        //result text displayed
-        $("#subBtn").on('click', function adventureWins() {  
-if ((adventureCount > comedyCount) && 
-    (adventureCount > actionCount) && 
+let genreList = {
+  adventureList,
+  actionList,
+  comedyList,
+  romanceList,
+  horrorList
+}
+
+// takes button and adds movie title, rating, plot, and poster.
+function displayMovie () {
+  let movie = $(this).attr("data-movie");
+
+  let queryURL = 'https://www.omdbapi.com/?t=' + movie + '&apikey=d56ade4e';
+
+  $.ajax({
+    url: queryURL,
+    method: 'GET'
+  }).then(function (response) {
+    let movieDiv = $("<div class='movie'>");
+    let title = response.Title;
+    let release = response.Released;
+    let rating = response.Rating;
+    let plot = response.Plot;
+    let imgURL = response.Poster;
+    let p = $("<p>").text(title);
+    let poster = $("<img>").attr("src", imgURL);
+    let pOne = $("<p>").text("Released: " + release);
+    let pTwo = $("<p>").text("Rating: " + rating);
+    let pThree = $("<p>").text("Plot: " + plot);
+    movieDiv.append(p);
+    movieDiv.append(poster);
+    movieDiv.append(pOne);
+    movieDiv.append(pTwo);
+    movieDiv.append(pThree);
+    $("#showMovie").prepend(movieDiv);
+    // movieDiv.append(pOne);
+    // movieDiv.append(pTwo);
+    // movieDiv.append(pThree);
+    // movieDiv.append(poster);
+    console.log(response);
+    
+  });
+}
+displayMovie();
+function movieButton() {
+  $('#buttons').empty();
+  
+  for (let i = 0; i < movies.length; i++) {
+
+  }
+}
+//variables set at 0 for each genre
+let adventureCount = 0;
+let comedyCount = 0;
+let actionCount = 0;
+let romanceCount = 0;
+let horrorCount = 0;
+
+let genre = undefined;
+
+//functions that increase the value of their genre
+function adventurePlus() {
+  adventureCount++;
+  console.log("adventureCount: " + adventureCount);
+}
+
+function comedyPlus() {
+  comedyCount++;
+  console.log("comedyCount: " + comedyCount);
+}
+
+function actionPlus() {
+  actionCount++;
+  console.log("actionCount: " + actionCount);
+}
+
+function romancePlus() {
+  romanceCount++;
+  console.log("romanceCount: " + romanceCount);
+}
+
+function horrorPlus() {
+  horrorCount++;
+  console.log("horrorCount: " + horrorCount);
+}
+
+//function that disables button from being clicked more than once
+function stopClick() {
+  $(this).prop('disabled', true);
+}
+
+//function that colors buttons when touched
+function toggle() {
+  $(this).css('opacity', '0.2');
+}
+
+
+//functions called on click
+$('#adventure, #adventure2').click(adventurePlus).click(toggle).click(stopClick);
+$('#action, #action2').click(actionPlus).click(toggle).click(stopClick);
+$('#comedy, #comedy2').click(comedyPlus).click(toggle).click(stopClick);
+$('#romance, #romance2').click(romancePlus).click(toggle).click(stopClick);
+$('#horror, #horror2').click(horrorPlus).click(toggle).click(stopClick);
+
+//result text displayed
+$("#subBtn").on('click', function adventureWins() {
+  if ((adventureCount > comedyCount) &&
+    (adventureCount > actionCount) &&
     (adventureCount > romanceCount) &&
     (adventureCount > horrorCount)
-    ){
-      $("img").hide();
-      $("#resultText").append("<p>Your movie genre is: Adventure</p>");
-      let genre = (adventureList);
-      console.log("Genre: " + genre);
-      $("#subBtn").hide();
-      movieFind(genre);
-      
-    }
-else if ((comedyCount > adventureCount) && 
-(comedyCount > actionCount) && 
-(comedyCount > romanceCount) &&
-(comedyCount > horrorCount)
-){
-  $("img").hide();
-  $("#resultText").append("<p>Your movie genre is: Comedy</p>");
-  let genre = ("Comedy");
-  console.log("Genre: " + genre);
-  $("#subBtn").hide();
-  
-}
-else if ((actionCount > adventureCount) && 
-(actionCount > comedyCount) && 
-(actionCount > romanceCount) &&
-(actionCount > horrorCount)
-){
-  $("img").hide();
-  $("#resultText").append("<p>Your movie genre is: Action</p>");
-  let genre = ("Action");
-  console.log("Genre: " + genre);
-  $("#subBtn").hide();
-}
-else if ((romanceCount > adventureCount) && 
-(romanceCount > actionCount) && 
-(romanceCount > comedyCount) &&
-(romanceCount > horrorCount)
-){
-  $("img").hide();
-  $("#resultText").append("<p>Your movie genre is: Romance</p>");
-  let genre = ("Romance");
-  console.log("Genre: " + genre);
-  $("#subBtn").hide();
-}
-else if ((horrorCount > adventureCount) && 
-(horrorCount > actionCount) && 
-(horrorCount > romanceCount) &&
-(horrorCount > comedyCount)
-){
-  $("img").hide();
-  $("#resultText").append("<p>Your movie genre is: Horror</p>");
-  let genre = ("Horror");
-  console.log("Genre: " + genre);
-  $("#subBtn").hide();
-}
+  ) {
+    $(".genreImg").hide();
+    $("#resultText").append("<p>Your movie genre is: Adventure</p>");
+    let genre = ("Adventure")
+    console.log("Genre: " + genre);
+    $("#subBtn").hide();
+
+  }
+  else if ((comedyCount > adventureCount) &&
+    (comedyCount > actionCount) &&
+    (comedyCount > romanceCount) &&
+    (comedyCount > horrorCount)
+  ) {
+    $(".genreImg").hide();
+    $("#resultText").append("<p>Your movie genre is: Comedy</p>");
+    let genre = ("Comedy");
+    console.log("Genre: " + genre);
+    $("#subBtn").hide();
+
+  }
+  else if ((actionCount > adventureCount) &&
+    (actionCount > comedyCount) &&
+    (actionCount > romanceCount) &&
+    (actionCount > horrorCount)
+  ) {
+    $(".genreImg").hide();
+    $("#resultText").append("<p>Your movie genre is: Action</p>");
+    let genre = ("Action");
+    console.log("Genre: " + genre);
+    $("#subBtn").hide();
+  }
+  else if ((romanceCount > adventureCount) &&
+    (romanceCount > actionCount) &&
+    (romanceCount > comedyCount) &&
+    (romanceCount > horrorCount)
+  ) {
+    $(".genreImg").hide();
+    $("#resultText").append("<p>Your movie genre is: Romance</p>");
+    let genre = ("Romance");
+    console.log("Genre: " + genre);
+    $("#subBtn").hide();
+  }
+  else if ((horrorCount > adventureCount) &&
+    (horrorCount > actionCount) &&
+    (horrorCount > romanceCount) &&
+    (horrorCount > comedyCount)
+  ) {
+    $(".genreImg").hide();
+    $("#resultText").append("<p>Your movie genre is: Horror</p>");
+    let genre = ("Horror");
+    console.log("Genre: " + genre);
+    $("#subBtn").hide();
+  }
 })
